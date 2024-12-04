e030de76ebd14139d7b7880907adafa2fb860d35
Ali Alsarhayd <wwali4ww@gmail.com>
2024-12-04 11:41:33 +0300

    Update HandicapParking.v

--- HandicapParking.v
+++ HandicapParking.v
@@ -5,18 +5,15 @@ module HandicapParking(input clk, reset, entry0, exit0,output reg [2:0] handicap
   Debounce dexit(clk, exit0, exit);
 
     always @(posedge clk) 
-    begin
         if (reset)
             handicap_spaces <= 3'b101;
-        end
-  else if (entry && handicap_spaces > 0)
+    
+        else if (entry && handicap_spaces > 0)
             handicap_spaces <= handicap_spaces - 1;
-        end
-        else if (exit && handicap_spaces < 5'b101)
+    
+        else if (exit && handicap_spaces < 3'b101)
             handicap_spaces <= handicap_spaces + 1;
-        end
-    end
-
+    
     assign handicap_green = (handicap_spaces >0);
     assign handicap_red = (handicap_spaces == 0);
 endmodule
