7ef5d92ef9699e60b7c717519a54f09d8e1943df
Ali Alsarhayd <wwali4ww@gmail.com>
2024-12-04 11:40:43 +0300

    Update NormalParking.v

--- NormalParking.v
+++ NormalParking.v
@@ -5,17 +5,12 @@ module NormalParking(input clk, reset, entry0, exit0,output reg [4:0] normal_spa
   Debounce dexit(clk, exit0, exit);
 
     always @(posedge clk) 
-    begin
         if (reset)
             normal_spaces <= 5'b10100;
-        end
         else if (entry && normal_spaces > 0)
             normal_spaces <= normal_spaces - 1;
-        end
         else if (exit && normal_spaces < 5'b10100)
             normal_spaces <= normal_spaces + 1;
-        end
-    end
 
     assign normal_green = (normal_spaces >0);
     assign normal_red = (normal_spaces == 0);
