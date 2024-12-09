c0557f3cd097c29c25cf98d1c35c6eef257ea4f9
Ali Alsarhayd <wwali4ww@gmail.com>
2024-12-09 15:11:06 +0300

    Create dsusf.v

---
+++ dsusf.v
@@ -0,0 +1,64 @@
+NET "seg[7]" IOSTANDARD = LVCMOS18;
+NET "seg[6]" IOSTANDARD = LVCMOS18;
+NET "seg[5]" IOSTANDARD = LVCMOS18;
+NET "seg[4]" IOSTANDARD = LVCMOS18;
+NET "seg[3]" IOSTANDARD = LVCMOS18;
+NET "seg[2]" IOSTANDARD = LVCMOS18;
+NET "seg[1]" IOSTANDARD = LVCMOS18;
+NET "seg[0]" IOSTANDARD = LVCMOS18;
+
+NET "seg[7]" LOC = H15;
+NET "seg[6]" LOC = L18;
+NET "seg[5]" LOC = T11;
+NET "seg[4]" LOC = P15;
+NET "seg[3]" LOC = K13;
+NET "seg[2]" LOC = K16;
+NET "seg[1]" LOC = R10;
+NET "seg[0]" LOC = T10;
+
+NET "an[7]" IOSTANDARD = LVCMOS18;
+NET "an[6]" IOSTANDARD = LVCMOS18;
+NET "an[5]" IOSTANDARD = LVCMOS18;
+NET "an[4]" IOSTANDARD = LVCMOS18;
+NET "an[3]" IOSTANDARD = LVCMOS18;
+NET "an[2]" IOSTANDARD = LVCMOS18;
+NET "an[1]" IOSTANDARD = LVCMOS18;
+NET "an[0]" IOSTANDARD = LVCMOS18;
+
+NET "an[7]" LOC = U13;
+NET "an[6]" LOC = K2;
+NET "an[5]" LOC = T14;
+NET "an[4]" LOC = P14;
+NET "an[3]" LOC = J14;
+NET "an[2]" LOC = T9;
+NET "an[1]" LOC = J18;
+NET "an[0]" LOC = J17;
+
+# PlanAhead Generated physical constraints 
+
+NET "clk" LOC = E3;
+
+# PlanAhead Generated IO constraints 
+
+NET "clk" IOSTANDARD = LVCMOS18;
+NET "h_entry" IOSTANDARD = LVCMOS18;
+NET "h_exit" IOSTANDARD = LVCMOS18;
+NET "H_green" IOSTANDARD = LVCMOS18;
+NET "H_red" IOSTANDARD = LVCMOS18;
+NET "n_entry" IOSTANDARD = LVCMOS18;
+NET "n_exit" IOSTANDARD = LVCMOS18;
+NET "N_green" IOSTANDARD = LVCMOS18;
+NET "N_red" IOSTANDARD = LVCMOS18;
+NET "reset" IOSTANDARD = LVCMOS18;
+
+# PlanAhead Generated physical constraints 
+
+NET "n_entry" LOC = M17;
+NET "n_exit" LOC = P18;
+NET "h_entry" LOC = M18;
+NET "h_exit" LOC = P17;
+NET "reset" LOC = N17;
+NET "N_green" LOC = M16;
+NET "H_green" LOC = R11;
+NET "N_red" LOC = N15;
+NET "H_red" LOC = N16;
