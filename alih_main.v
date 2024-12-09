2a36dc0f7cf8b3bd7a7654691012e13bb564f8d6
Ali Alsarhayd <wwali4ww@gmail.com>
2024-12-09 10:33:04 +0300

    Update alih_main.v

--- alih_main.v
+++ alih_main.v
@@ -1,1 +1,11 @@
 
+module SmartParkingSystem (input clk, input reset, input n_entry, input n_exit, input h_entry, input h_exit, output [6:0] seg_n, output [6:0] seg_h, output N_green, output N_red, output H_green, output H_red);
+  wire [4:0] N_count;
+  wire [2:0] H_count;
+    wire N_full, H_full;
+  
+  HandicappedParking hp(clk, reset, h_entry, h_exit, H_count, H_full, H_green, H_red);
+  
+  NormalParking np(clk, reset, n_entry, n_exit, N_count, N_full, N_green, N_red);
+  
+  DISP7SEG sev();
